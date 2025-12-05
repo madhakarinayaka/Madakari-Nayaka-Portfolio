afff4477a8d113abcb78cedda74b7bec26fc5506
D MADAKARI NAYAKA <91912884+madhakarinayaka@users.noreply.github.com>
2025-12-05 15:20:04 +0530

    Update index.js

--- css and js/index.js
+++ css and js/index.js
@@ -55,7 +55,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 // change js
 
 var typed = new Typed(".multiple-text", {
-    strings:["Frontend Developer", "Backend Developer"],
+    strings:["PL/SQL Developer", "Backend Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
@@ -92,3 +92,4 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
